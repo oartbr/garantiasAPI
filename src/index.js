91a72160780a70f935f38983f1d586689984e415
Alex Sevilla <alex.sevilla@gmail.com>
2024-08-18 15:53:27 -0300

    chore: retrieve data from mongodb and include in logger message

--- src/index.js
+++ src/index.js
@@ -2,13 +2,16 @@ const mongoose = require('mongoose');
 const app = require('./app');
 const config = require('./config/config');
 const logger = require('./config/logger');
+const { User } = require('./models');
 
 let server;
-mongoose.connect(config.mongoose.url, config.mongoose.options).then(() => {
+mongoose.connect(config.mongoose.url, config.mongoose.options).then(async () => {
   logger.info('Connected to MongoDB');
-  mongoose.connection.on('connected', () => {
-    logger.info('>>>> Confirmed connection to MongoDB <<<<<');
-  });
+  // Get data from MongoDB
+  const data = await User.find();
+
+  // Include data in logger message
+  logger.info(`Retrieved data from MongoDB: ${data}`);
   logger.info(`Listening to port ${config.mongoose.url} :: ${config.mongoose.options}`);
   server = app.listen(config.port, () => {
     logger.info(`Listening to port ${config.port}`);
