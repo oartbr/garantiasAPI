19973e0e42091d8912bb2ecba2e5027d7978d7f3
Alex Sevilla <alex.sevilla@gmail.com>
2024-08-18 15:44:32 -0300

    chore: add confirmation message for mongodb connection

--- src/index.js
+++ src/index.js
@@ -6,6 +6,9 @@ const logger = require('./config/logger');
 let server;
 mongoose.connect(config.mongoose.url, config.mongoose.options).then(() => {
   logger.info('Connected to MongoDB');
+  mongoose.connection.on('connected', () => {
+    logger.info('>>>> Confirmed connection to MongoDB <<<<<');
+  });
   logger.info(`Listening to port ${config.mongoose.url} :: ${config.mongoose.options}`);
   server = app.listen(config.port, () => {
     logger.info(`Listening to port ${config.port}`);
