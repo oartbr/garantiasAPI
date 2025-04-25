50ca5cbc166e98883fcd028fd10463f07c92953e
Alex Sevilla <alex.sevilla@gmail.com>
2025-04-24 23:20:55 -0300

    feat: enhance QR code generation by adjusting dimensions and logo integration

--- src/services/qrcode.service.js
+++ src/services/qrcode.service.js
@@ -10,7 +10,7 @@ const axios = require('axios');
 const getQRcode = async (content) => {
   return QRCode.toString(`${content}`, [{ data: content, mode: 'byte' }], {
     type: 'svg',
-    scale: 1,
+    scale: 4,
     error: 'H',
   });
 };
@@ -25,25 +25,25 @@ const insertLogo = async (QRcode, logoBG) => {
   // Load the QR code SVG into JSDOM
   const dom = new JSDOM(QRcode);
   const { document } = dom.window;
-  const svgElem = document.querySelector('svg');
-  svgElem.querySelectorAll('*').forEach((el) => el.classList.add('originalQR'));
-
-  // Clone the original QR code element and add a marker class for cleanup.
-  const qrSVG = svgElem.cloneNode(true);
+  const qrElem = document.querySelector('svg');
+  // qrElem.querySelectorAll('*').forEach((el) => el.classList.add('originalQR'));
 
   // Fetch the logo SVG.
   const logoDom = new JSDOM(logoBG);
   const logoSvg = logoDom.window.document.querySelector('svg');
 
   // Use logo dimensions for setting up the area.
-  const svgWidth = parseFloat(logoSvg.getAttribute('width')) || 10;
-  const svgHeight = parseFloat(logoSvg.getAttribute('height')) || 10;
+  const svgWidth = parseFloat(logoSvg.getAttribute('width')) || 1000;
+  const svgHeight = parseFloat(logoSvg.getAttribute('height')) || 1000;
   // If no explicit width/height, implement viewBox to default.
 
   // Update the original QR code SVG dimensions and viewBox.
-  svgElem.setAttribute('width', svgWidth);
-  svgElem.setAttribute('height', svgHeight);
-  svgElem.setAttribute('viewBox', `0 0 ${svgWidth} ${svgHeight}`);
+  qrElem.setAttribute('x', '0%');
+  qrElem.setAttribute('y', '0%');
+  qrElem.setAttribute('width', svgWidth);
+  qrElem.setAttribute('height', svgHeight);
+  qrElem.setAttribute('viewBox', `2 -3 ${svgWidth * 0.4} ${svgHeight * 0.4}`);
+  qrElem.setAttribute('preserveAspectRatio', 'xMinYMin meet');
   // console.log(`New dimensions: width=${svgWidth}, height=${svgHeight}`);
 
   // Adjust the logo size and its placement.
@@ -51,23 +51,13 @@ const insertLogo = async (QRcode, logoBG) => {
   logoSvg.setAttribute('y', '0%');
   logoSvg.setAttribute('width', '100%');
   logoSvg.setAttribute('height', '100%');
-
-  // Adjust the cloned QR code size and position.
-  qrSVG.setAttribute('x', '-3%');
-  qrSVG.setAttribute('y', '5%');
-  qrSVG.setAttribute('width', svgWidth * 2.6);
-  qrSVG.setAttribute('height', svgHeight * 2.6);
-  qrSVG.setAttribute('viewBox', `0 0 ${svgWidth} ${svgHeight}`);
-  qrSVG.setAttribute('fill', '#66ffffff');
-
-  // Remove all elements with the marker class.
-  svgElem.querySelectorAll('.originalQR').forEach((el) => el.remove());
+  logoSvg.setAttribute('viewBox', `0 0 ${svgWidth} ${svgHeight}`);
+  logoSvg.setAttribute('preserveAspectRatio', 'xMinYMin meet');
 
   // Append the modified QR clone and the logo to the original SVG.
-  svgElem.appendChild(qrSVG);
-  svgElem.appendChild(logoSvg);
+  logoSvg.prepend(qrElem);
 
-  return svgElem.outerHTML;
+  return logoSvg.outerHTML;
 };
 
 /**
@@ -82,15 +72,15 @@ const insertLogo = async (QRcode, logoBG) => {
 const insertText = (svgString, textContent) => {
   const dom = new JSDOM(svgString);
   const { document } = dom.window;
-  const svgElem = document.querySelector('svg');
+  const qrElem = document.querySelector('svg');
   const monospacedFont = 'Consolas, "Courier New", monospace';
 
   // Determine the SVG dimensions.
-  let svgWidth = svgElem.getAttribute('width');
-  let svgHeight = svgElem.getAttribute('height');
+  let svgWidth = qrElem.getAttribute('width');
+  let svgHeight = qrElem.getAttribute('height');
   if (!svgWidth || !svgHeight) {
     // If no explicit width/height, try viewBox or default to 200.
-    const viewBox = svgElem.getAttribute('viewBox');
+    const viewBox = qrElem.getAttribute('viewBox');
     if (viewBox) {
       const [, , width, height] = viewBox.split(' '); // we are getting the 2nd and 3rd values of the viewBox
       svgWidth = width;
@@ -119,7 +109,7 @@ const insertText = (svgString, textContent) => {
   rectElem.setAttribute('fill', '#fff');
 
   // Append the rectangle to the SVG.
-  svgElem.appendChild(rectElem);
+  qrElem.appendChild(rectElem);
 
   // Create the text element.
   const textElem = document.createElementNS('http://www.w3.org/2000/svg', 'text');
@@ -134,9 +124,9 @@ const insertText = (svgString, textContent) => {
   textElem.setAttribute('font-size', '2'); // Adjust font size as needed
 
   // Append text on top of the rectangle.
-  svgElem.appendChild(textElem);
+  qrElem.appendChild(textElem);
 
-  return svgElem.outerHTML;
+  return qrElem.outerHTML;
 };
 
 /**
